d63e5c4439305fbd12a365dab66bab662b102287
jacob ruleaux and Michelle Poterek <michelle@>
2017-07-06 16:17:38 -0700

    jacob is a badass winkey face master

--- src/NumberWord.php
+++ src/NumberWord.php
@@ -4,10 +4,35 @@
 
     {
 
+        function stringLength($input)
+        {
+            $length_of_string = strlen($input);
+            return $length_of_string;
+        }
+
+
+
+
         function oneToNine($input)
         {
-            if ($input = "1") {
+            if ($input == "1") {
                 return "one";
+            } elseif ($input == "2" ) {
+                return "two";
+            } elseif ($input == "3" ) {
+                return "three";
+            } elseif ($input == "4" ) {
+                return "four";
+            } elseif ($input == "5" ) {
+                return "five";
+            } elseif ($input == "6" ) {
+                return "six";
+            } elseif ($input == "7" ) {
+                return "seven";
+            } elseif ($input == "8" ) {
+                return "eight";
+            } elseif ($input == "9" ) {
+                return "nine";
             }
         }
 
--- tests/NumberWordTest.php
+++ tests/NumberWordTest.php
@@ -1,17 +1,28 @@
 <?php
     require_once "src/NumberWord.php";
-    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    class NumberWordTest extends PHPUnit_Framework_TestCase
     {
 
         function testNumberOnes()
 
         {
             $test_number_word = new NumberWord;
-            $input = "1";
+            $input = "9";
 
             $result = $test_number_word->oneToNine($input);
 
-            $this->assertEquals("one", $result);
+            $this->assertEquals("nine", $result);
+        }
+
+        function testStringLength()
+        {
+            $test_length_of_string = new NumberWord;
+            $input = "333";
+
+            $result = $test_length_of_string->stringLength($input);
+
+            $this->assertEquals(3, $result);
+
         }
 
 
